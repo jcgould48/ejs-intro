5263c23ddf439c7be360210a1e62560b5554c684
Jesse Gould <jesse.gould@codeimmersives.com>
2020-02-12 14:35:16 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express()
+const logger = require('morgan')
+const path = require('path')
+const Users = require ('./models/Users.json')
+
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// app.get('/first', (req, res)=>{
+
+//     res.render('main/home', {name: 'Bill'})
+// });
+
+app.get('/users/', (req, res)=>{
+      res.render('main/users', {Users});
+});
+
+
+// app.get('/location/:color/:car', (req, res)=>{
+//     const firstName = "john"
+//     const lastName = "bill"
+//     let places = [
+//         {city: "ss", state:'Ct'},
+//         {city: "ab", state:'Ct'},
+//         {city: "bb", state:'fl'},
+
+//     ]
+//     const {color, car} = req.params;
+//     res.render('main/location', {color, car, places, firstName, lastName});
+// });
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+    });
